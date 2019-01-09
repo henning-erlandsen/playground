5dcd5df66cb337217f9fd1c01cb0ebe461b564d4
Henning Erlandsen <henning.erlandsen@mojang.com>
2019-01-09 22:47:34 +0100

    Add empty google test runnner

--- modules/test/main.cpp
+++ modules/test/main.cpp
@@ -1,8 +1,8 @@
 
-#include <cstdio>
+#include "gtest/gtest.h"
 
 int main(int argc, char** argv)
 {
-    printf("hello workdsl:");
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
